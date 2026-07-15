cf00597621a6ae17d3aec8552f32350a4a5af939
Gabriel Teixeira <teixeirag.dev@gmail.com>
2026-07-15 11:53:50 -0300

    Mudando nomenclatura do package

---
+++ backend/src/test/java/com/woodsuite/teixeiradev/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodsuite.teixeiradev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
